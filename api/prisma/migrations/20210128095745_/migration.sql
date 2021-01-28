6cecc743f1f1d77083003ca2ddcba25320a16efe
Sam Thomson <samt@samt.st>
2021-01-28 15:03:56 +0000

    remove prisma schema + migrations

--- api/prisma/migrations/20210128095745_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `nomicsId` to the `Currency` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Currency` ADD COLUMN     `nomicsId` VARCHAR(191) NOT NULL;
