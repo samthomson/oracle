be7487bd0f7fe8992440a4d2c9691082b89c67b8
Sam Thomson <samt@samt.st>
2021-01-28 15:05:33 +0000

    get data storing and pulling working

--- api/src/pull-data.ts
+++ api/src/pull-data.ts
@@ -1,23 +1,23 @@
 // import { getValues } from './services/cmc-data'
 import { getValues } from './services/nomics-data'
 import * as DB from './util/db'
-import * as DBS from './util/dbS'
+import * as DBS from './util/SequelizeDB'
+import SequelizeDB from './db/setup'
+import * as SequelizeDatabaseModels from './db/setup'
 
-const test = async () => {
+const testRun = async () => {
+    // ensure tables exist
+    await SequelizeDB.sync()
+
     const values = await getValues()
 
-    const firstValues = values.filter((v, i) => i < 5000)
+    // const firstValues = values.filter((v, i) => i < 5000)
 
     console.log(values.length, ' currencies received')
 
-    // firstValues.forEach((cur) => {
-    //     console.log(`sym: ${cur.symbol}, BTC price: ${cur.quote.BTC.price}`)
-    // })
+    const currencies = values.map((cur) => ({ id: cur.id, name: cur.name, symbol: cur.symbol, price: cur.price }))
 
-    const ensureCurrenciesExistInput = {
-        currencies: firstValues.map((cur) => ({ id: cur.id, name: cur.name, symbol: cur.symbol })),
-    }
-
+    /*
     const uniqueItems = [...new Set(firstValues.map((it) => it.symbol))]
 
     console.log('unique count: ', uniqueItems.length)
@@ -32,11 +32,44 @@ const test = async () => {
     const duplicates = Object.keys(counts).filter((sym) => counts[sym] > 1)
 
     console.log(duplicates)
+    */
+    const logEntry = await DBS.createLogEntry()
 
-    const added = await DBS.ensureCurrenciesExistInDB(ensureCurrenciesExistInput)
+    for (let i = 0; i < currencies.length; i++) {
+        const currency = await DBS.ensureCurrencyExists(currencies[i])
+        await DBS.createCurrencyEntry(currency.id, logEntry.id, currencies[i])
+    }
 
-    console.log('\n\nall done', added, ' records added')
+    // const added = await DBS.ensureCurrenciesExistInDB(ensureCurrenciesExistInput)
+    logEntry.currenciesSaved = currencies.length
+    logEntry.save()
+
+    console.log('\n\nall done')
+}
+
+const testRelations = async () => {
+    // get currency entries for 'ABBC'
+    await SequelizeDB.sync()
+
+    const abbc = await SequelizeDatabaseModels.Currency.findOne({
+        where: { nomicsId: 'ABBC' },
+        include: SequelizeDatabaseModels.CurrencyEntry,
+    })
+
+    const latestLogEntry = await SequelizeDatabaseModels.LogEntry.findOne({
+        include: SequelizeDatabaseModels.CurrencyEntry,
+    })
+
+    console.log(abbc)
+    console.log(abbc.currency_entries.length, 'entries for this currency')
+    console.log(latestLogEntry.currency_entries.length, 'entries for this log entry')
+    // console.log(latestLogEntry.currency_entries.length, 'entries for this currency')
+
+    // const { entries } = abbc
+
+    // console.log(entries.length)
 }
 
 console.log('\ndata test stub')
-test()
+// testRun()
+testRelations()
