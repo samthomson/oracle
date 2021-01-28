af1e5e1a6dbfbd434113327cfcaf1aa8dc49d600
Sam Thomson <samt@samt.st>
2021-01-28 22:49:01 +0000

    2nd minute test

--- api/src/pseudo-cron.ts
+++ api/src/pseudo-cron.ts
@@ -2,6 +2,6 @@ import { CronJob } from 'cron'
 import { pullData } from './pull-data'
 
 // every fifth minute
-const cronFrequency = '* * * * *'
+const cronFrequency = '*/2 * * * *'
 
 new CronJob(cronFrequency, () => pullData(), undefined, true, 'Europe/London')
