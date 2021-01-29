9ef7c8e156cddfd0085ada204d2bea1e52d89245
Sam Thomson <samt@samt.st>
2021-01-29 11:29:33 +0000

    use env var for db host

--- api/src/db/setup.ts
+++ api/src/db/setup.ts
@@ -1,7 +1,7 @@
 import * as Sequelize from 'sequelize'
 
 const dbConfig = {
-    host: 'mysql',
+    host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DATABASE,
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
