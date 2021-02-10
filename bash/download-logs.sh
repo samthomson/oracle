aeee9bb1c01cccf3097968e521272037647617d5
Sam Thomson <samt@samt.st>
2021-02-10 13:09:42 +0000

    tweek logging download

--- bash/download-logs.sh
+++ bash/download-logs.sh
@@ -6,6 +6,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 source $SCRIPTPATH/../.env
 
-echo "will pull logs from docker-machine host: DOCKER_MACHINE_NAME"
+echo "will pull logs from docker-machine host: $DOCKER_MACHINE_NAME"
 
-docker-machine scp -r -d DOCKER_MACHINE_NAME:/serverlogs/ ./serverlogs
+docker-machine scp -r -d $DOCKER_MACHINE_NAME:/serverlogs/ ./serverlogs
