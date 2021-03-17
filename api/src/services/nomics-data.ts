3e10e80cad74bff2790922ca6172ec32538c37a4
Sam Thomson <samt@samt.st>
2021-03-17 19:13:58 +0000

    fix nomics issue

--- api/src/services/nomics-data.ts
+++ api/src/services/nomics-data.ts
@@ -3,50 +3,69 @@ import * as Types from '../declarations'
 import Logger from './logging'
 import moment from 'moment'
 
-type NomicsListingPage = {
-    listings: Types.NomicsListing[]
-    totalItems: number
-}
+// type NomicsListingPage = {
+//     listings: Types.NomicsListing[]
+//     totalItems: number
+// }
+
+// export const getValues = async (): Promise<Types.NomicsListing[]> => {
+//     try {
+//         let allListings: Types.NomicsListing[] = []
+//         let page = 1
+//         let hasMore = true
+
+//         const startTime = moment()
+//         while (hasMore) {
+//             const { listings, totalItems } = await getPageOfValues(page)
+//             hasMore = Math.ceil(totalItems / 100) > page
+//             allListings = [...allListings, ...listings]
+//             page++
+//         }
+//         const endTime = moment()
+
+//         const milliseconds = endTime.diff(startTime)
+//         Logger.info(`time spent assembling nomics data: ${milliseconds.toLocaleString()} ms`)
+
+//         return allListings
+//     } catch (err) {
+//         Logger.error('error assembling nomics data', err)
+//         return []
+//     }
+// }
+
+// const getPageOfValues = async (page: number): Promise<NomicsListingPage> => {
+//     const url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.NOMICS_API_KEY}&convert=BTC&per-page=100&status=active&page=${page}`
+//     const response = await fetch(url)
+
+//     if (response.status !== 200) {
+//         Logger.error('getPageOfValues error', { response, status: response?.status, url })
+//         throw new Error('Error fetching Nomics data')
+//     }
+//     const totalItems: number = parseInt(response.headers.get('X-Pagination-Total-Items'))
+
+//     const listings: Types.NomicsListing[] = await response.json()
+
+//     return {
+//         listings,
+//         totalItems,
+//     }
+// }
 
 export const getValues = async (): Promise<Types.NomicsListing[]> => {
     try {
-        let allListings: Types.NomicsListing[] = []
-        let page = 1
-        let hasMore = true
+        const url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.NOMICS_API_KEY}&convert=BTC&status=active`
+        const response = await fetch(url)
 
-        const startTime = moment()
-        while (hasMore) {
-            const { listings, totalItems } = await getPageOfValues(page)
-            hasMore = Math.ceil(totalItems / 100) > page
-            allListings = [...allListings, ...listings]
-            page++
+        if (response.status !== 200) {
+            Logger.error('getPageOfValues error', { response, status: response?.status, url })
+            throw new Error('Error fetching Nomics data')
         }
-        const endTime = moment()
 
-        const milliseconds = endTime.diff(startTime)
-        Logger.info(`time spent assembling nomics data: ${milliseconds.toLocaleString()} ms`)
+        const listings: Types.NomicsListing[] = await response.json()
 
-        return allListings
+        return listings
     } catch (err) {
         Logger.error('error assembling nomics data', err)
         return []
-    }
-}
-
-const getPageOfValues = async (page: number): Promise<NomicsListingPage> => {
-    const url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.NOMICS_API_KEY}&convert=BTC&per-page=100&status=active&page=${page}`
-    const response = await fetch(url)
-
-    if (response.status !== 200) {
-        Logger.error('getPageOfValues error', { response, status: response?.status, url })
-        throw new Error('Error fetching Nomics data')
-    }
-    const totalItems: number = parseInt(response.headers.get('X-Pagination-Total-Items'))
-
-    const listings: Types.NomicsListing[] = await response.json()
-
-    return {
-        listings,
-        totalItems,
     }
 }
