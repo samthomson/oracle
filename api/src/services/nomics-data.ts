cd20f0079cd1d369860231e1cc2ca08949f859fe
Sam Thomson <samt@samt.st>
2021-03-17 14:02:29 +0000

    more logging on failing requests

--- api/src/services/nomics-data.ts
+++ api/src/services/nomics-data.ts
@@ -34,12 +34,11 @@ export const getValues = async (): Promise<Types.NomicsListing[]> => {
 }
 
 const getPageOfValues = async (page: number): Promise<NomicsListingPage> => {
-    const response = await fetch(
-        `https://api.nomics.com/v1/currencies/ticker?key=${process.env.NOMICS_API_KEY}&convert=BTC&per-page=100&status=active&page=${page}`,
-    )
+    const url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.NOMICS_API_KEY}&convert=BTC&per-page=100&status=active&page=${page}`
+    const response = await fetch(url)
 
     if (response.status !== 200) {
-        Logger.error('getPageOfValues error', response)
+        Logger.error('getPageOfValues error', { response, status: response?.status, url })
         throw new Error('Error fetching Nomics data')
     }
     const totalItems: number = parseInt(response.headers.get('X-Pagination-Total-Items'))
