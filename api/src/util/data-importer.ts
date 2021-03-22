15cdfb7c33900a4b0fa1f811832d470b8d11de4a
Sam Thomson <samt@samt.st>
2021-03-22 10:33:05 +0000

    refactor data importer

--- api/src/util/data-importer.ts
+++ api/src/util/data-importer.ts
@@ -3,8 +3,6 @@ import * as NomicsService from '../services/nomics-data'
 import * as BittrexService from '../services/bittrex-data'
 import * as DBUtil from './SequelizeDB'
 import * as HelperUtil from '../util/helper'
-import * as Constants from '../constants'
-// import * as SequelizeDatabase from './db/connection'
 
 export const pullNomicsData = async () => {
     // ensure tables exist
@@ -19,11 +17,11 @@ export const pullNomicsData = async () => {
         symbol: cur.symbol,
         price: cur.price,
         quote: 'BTC',
-        sourceId: Constants.Source.Nomics,
+        sourceId: Types.Constants.Source.Nomics,
     }))
 
     // create an initial log entry in the db, so we have an id to relate other models against
-    const logEntry = await DBUtil.createLogEntry(0)
+    const logEntry = await DBUtil.createLogEntry(Types.Constants.Source.Nomics)
 
     // for each currency, ensure it exists in the db, then store a currency price against it
     for (let i = 0; i < draftMarkets.length; i++) {
@@ -42,29 +40,22 @@ export const pullBittrexData = async () => {
     // pull all currency data
     const bittrexTickers = await BittrexService.getValues()
 
-    console.log(bittrexTickers.length)
-
     // filter down to just the data we need
     const draftMarkets: Types.DraftMarket[] = bittrexTickers.map((cur) => {
         const { symbol, quote } = HelperUtil.bittrexV3MarketSymbolToQuoteSymbolObject(cur.symbol)
 
         return {
-            // id: cur.id,
             name: cur.symbol,
             symbol,
             price: cur.lastTradeRate,
             quote,
-            sourceId: Constants.Source.Bittrex,
+            sourceId: Types.Constants.Source.Bittrex,
         }
     })
-    console.log(draftMarkets)
 
-    /*
-    
     // create an initial log entry in the db, so we have an id to relate other models against
-    const logEntry = await DBUtil.createLogEntry(0)
-
-    // for each currency, ensure it exists in the db, then store a currency price against it
+    const logEntry = await DBUtil.createLogEntry(Types.Constants.Source.Bittrex)
+    // for each market, ensure it exists in the db, then store a market rate against it
     for (let i = 0; i < draftMarkets.length; i++) {
         const currency = await DBUtil.ensureMarketExists(draftMarkets[i])
         await DBUtil.createCurrencyEntry(currency.id, logEntry.id, draftMarkets[i])
@@ -73,8 +64,6 @@ export const pullBittrexData = async () => {
     // wrap up our log entry with the amount of currency entries created
     logEntry.currenciesSaved = draftMarkets.length
     logEntry.save()
-
-    */
 
     console.log('\n\nall done')
 }
