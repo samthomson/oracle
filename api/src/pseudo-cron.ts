df7e381ef911473b5067673ae98ec71facf95bc9
Sam Thomson <samt@samt.st>
2021-03-24 20:51:27 +0000

    add data crunching to cron task

--- api/src/pseudo-cron.ts
+++ api/src/pseudo-cron.ts
@@ -1,8 +1,18 @@
 import { CronJob } from 'cron'
 import * as DataImporter from './util/data-importer'
+import * as DataCruncher from './util/data-cruncher'
 
 // every fifth minute
 const cronFrequency = '*/5 * * * *'
 
 new CronJob(cronFrequency, () => DataImporter.pullNomicsData(), undefined, true, 'Europe/London')
-new CronJob(cronFrequency, () => DataImporter.pullBittrexData(), undefined, true, 'Europe/London')
+new CronJob(
+    cronFrequency,
+    async () => {
+        await DataImporter.pullBittrexData()
+        await DataCruncher.crunchBittrexMarkets()
+    },
+    undefined,
+    true,
+    'Europe/London',
+)
