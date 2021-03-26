d4cadbcce0a65d0f1ad73800d8a0eb986dcbc716
Sam Thomson <samt@samt.st>
2021-03-26 13:57:31 +0000

    add exception handling to bittrex importer

--- api/src/tasks/import-crunch-bittrex.ts
+++ api/src/tasks/import-crunch-bittrex.ts
@@ -4,14 +4,18 @@ import * as DataCruncher from '../util/data-cruncher'
 import Logger from '../services/logging'
 
 export const importCrunch = async () => {
-    const startTime = moment()
-    await DataImporter.pullBittrexData()
-    await DataCruncher.crunchBittrexMarkets()
+    try {
+        const startTime = moment()
+        await DataImporter.pullBittrexData()
+        await DataCruncher.crunchBittrexMarkets()
 
-    const endTime = moment()
-    const milliseconds = endTime.diff(startTime)
+        const endTime = moment()
+        const milliseconds = endTime.diff(startTime)
 
-    Logger.info(`3. time spent importing & crunching bittrex markets: ${milliseconds.toLocaleString()} ms`)
+        Logger.info(`3. time spent importing & crunching bittrex markets: ${milliseconds.toLocaleString()} ms`)
+    } catch (err) {
+        Logger.error('error importing and crunching bittrex data', err)
+    }
 }
 
 importCrunch()
