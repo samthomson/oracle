cfce031367118d95e8392a7897bd4cd283384564
Sam Thomson <samt@samt.st>
2021-04-05 21:03:33 +0100

    log requests

--- api/src/util/helper.ts
+++ api/src/util/helper.ts
@@ -1,3 +1,5 @@
+import * as Models from '../db/models'
+
 export const delay = async (ms: number): Promise<void> => {
     // return await for better async stack trace support in case of errors.
     return await new Promise((resolve) => setTimeout(resolve, ms))
@@ -16,4 +18,29 @@ export const calculateAverage = (list: number[]) => {
         return list.reduce((prev, curr) => prev + curr) / list.length
     }
     return undefined
+}
+
+const serviceStringToInt = (service: string): number | null => {
+    switch (service) {
+        case 'Bittrex':
+            return 0
+        default:
+            return null
+    }
+}
+
+export const logRequest = async (
+    service: 'Bittrex',
+    requestUrl: string,
+    params: string | null = null,
+): Promise<void> => {
+    const url = requestUrl.substring(0, 255)
+
+    const serviceInt = serviceStringToInt(service)
+
+    await Models.RequestLog.create({
+        serviceInt,
+        url,
+        params,
+    })
 }
