47245e242d179329d46fec5747c0034c3d121542
Sam Thomson <samt@samt.st>
2021-04-29 18:17:32 +0100

    rename

--- api/src/pseudo-cron.ts
+++ api/src/pseudo-cron.ts
@@ -1,16 +1,14 @@
 import { CronJob } from 'cron'
-import * as DataImporter from './util/data-importer'
-// import * as DataCruncher from './util/data-cruncher'
-import * as BittrexImporter from './tasks/import-crunch-bittrex'
+import * as ImportCrunch from './tasks/import-crunch-markets'
 import * as LogDataAges from './tasks/log-data-ages'
 
 // every hour: calculate ten hour MA
 const hourly = '0 * * * *'
-new CronJob(hourly, () => BittrexImporter.importCrunch(false), undefined, true, 'Europe/London')
+new CronJob(hourly, () => ImportCrunch.importCrunch(false), undefined, true, 'Europe/London')
 
 // every three minutes: calculate half hour and instant MA
 const everyFewMinutes = '*/3 * * * *'
-new CronJob(everyFewMinutes, () => BittrexImporter.importCrunch(true), undefined, true, 'Europe/London')
+new CronJob(everyFewMinutes, () => ImportCrunch.importCrunch(true), undefined, true, 'Europe/London')
 
 // log data ages
 const everyMinute = '* * * * *'
